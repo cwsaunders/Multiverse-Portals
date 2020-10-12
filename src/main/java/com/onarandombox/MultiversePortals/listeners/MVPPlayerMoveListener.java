bb5692cb5261fcb6b1a69ec3fdcdad10776c117a
Kermina Awad <kerminaawad@gmail.com>
2020-10-12 13:28:07 -0400

    check for legacy portals in the move event

--- src/main/java/com/onarandombox/MultiversePortals/listeners/MVPPlayerMoveListener.java
+++ src/main/java/com/onarandombox/MultiversePortals/listeners/MVPPlayerMoveListener.java
@@ -55,10 +55,10 @@ public class MVPPlayerMoveListener implements Listener {
             return;
         }
         MVPortal portal = ps.getStandingInPortal();
-        // If the portal is not null
-        // AND if we did not show debug info, do the stuff
-        // The debug is meant to toggle.
-        if (portal != null && ps.doTeleportPlayer(MoveType.PLAYER_MOVE) && !ps.showDebugInfo()) {
+        // If the portal is not null, and it's a legacy portal,
+        // and if we didn't show debug info, do the stuff.
+        // (the debug is meant to toggle)
+        if (portal != null && portal.isLegacyPortal() && ps.doTeleportPlayer(MoveType.PLAYER_MOVE) && !ps.showDebugInfo()) {
             MVDestination d = portal.getDestination();
             if (d == null) {
                 return;
